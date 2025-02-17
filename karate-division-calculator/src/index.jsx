43d831db9bbb54438c44ea1d3137139bd0d553b9
herrkutt <herrkutt@gmail.com>
2025-02-16 21:20:21 -0500

    Added new app

--- karate-division-calculator/src/index.jsx
+++ karate-division-calculator/src/index.jsx
@@ -1,12 +1,13 @@
 // src/index.js
 import './index.css';
+import './output.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename="/KarateScorer/karate-division-calculator">
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
